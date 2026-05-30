3d7fdb0e29d786ef762bbed4de4fe91778b3dc6e
michia <michiagayepadilla@gmail.com>
2026-05-30 09:28:24 -0400

    "updated button command"

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const answers = [
+  "How Should I Know",
+  "You Should Ask Your Therapist",
+  "Why are you asking a inanimate object about your Future? ",
+];
 
+const button = document.getElementById("button");
+console.log(" ");
 
-window.prompt("Ask me a question");
+button.addEventListener("click", showAsnwer);
+
+function showAsnwer() {
+   
+    document.getElementById("answer").textContent= " N/A ";
+}
