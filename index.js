01a637ece6bfc97820e477a4622990dcde3d8835
michia <michiagayepadilla@gmail.com>
2026-05-30 22:13:36 -0400

    "created random answer array"

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+const output = document.getElementById("output");
+
 const answers = [
   "How Should I Know",
   "You Should Ask Your Therapist",
   "Why are you asking a inanimate object about your Future? ",
 ];
 
+let index = 0;
+
+// click function to the button
 const button = document.querySelector("button");
 
-button.addEventListener("click", showAsnwer);
-{
-  console.log(button);
-}
+// show reply when button click
+button.addEventListener("click", submit);
 
-function showAsnwer() {
-    alert('Try Again')
 
+// alert reply
+
+
+function submit() {
+  const randomIndex = Math.floor(Math.random() * answers.length);
+  const response = answers[randomIndex];
+  document.getElementById("answers").textContent = response;
 }
 
